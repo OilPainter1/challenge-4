d7383963a3577e356ea1dfaeb175452025156329
Henry Cryns <henrycryns@MacBook-Air.local>
2022-09-22 13:14:45 -0500

    implemented event listener and quiz starts

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 var btn = document.getElementById("button")
 
+var questions = [
+    "What type of javascript data type is: \"true\"?",
+    "What does DOM stand for?",
+    "What does API stand for?",
+    "What is the symbol for multiplication in javascript?",
+    "What is the logical operator for \"not\"?",
+    "What is the logical operator for \"and\"?",
+    "What is the logical operator for \"or\"?",
+    "What is the exression for \"increment\"?"
+]
+
+
 
 btn.addEventListener("click",function(){
-    console.log("hello")
+    var btnC= document.querySelector("#button")
+    document.querySelector("#button-div").removeChild(btnC)
+    var div1 = document.createElement('div')
+    document.querySelector(".questions").appendChild(div1).textContent=questions[0]
+    
+
 })
+
+
+
+
+
+
+
